8a50a5bcad9df1054de2539a9102afbd353b76a2
Jeyw1n <j3ywin@gmail.com>
2024-12-09 13:33:47 +0300

    Added const with text for message

--- handlers/start.py
+++ handlers/start.py
@@ -2,10 +2,19 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
 
+from .markups import devices_list_markup
+
+
 start_router = Router(name=__name__)
+
+MESSAGE_TEXT = '👋 Здравствуйте! Мы готовы помочь вам с ремонтом.\n' \
+               'Пожалуйста, выберите устройство, которое нуждается в ' \
+               'обслуживании, и мы обработаем ваш запрос.' 
 
 
 @start_router.message(Command('start'))
 async def start_command(msg: Message) -> None:
-    user_id = msg.from_user.id
-    await msg.answer(f'Hi, your id is: {user_id}')
+    await msg.answer(
+        text=MESSAGE_TEXT,
+        reply_markup=devices_list_markup()
+    )
