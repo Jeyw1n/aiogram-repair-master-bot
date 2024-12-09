bd4ddccfd19755bb066a53a8dce7c1c32016025f
Jeyw1n <j3ywin@gmail.com>
2024-12-09 13:32:07 +0300

    Added deveces_list_markup

---
+++ handlers/markups.py
@@ -0,0 +1,35 @@
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+DEVICES = {
+    'Телефон 📱': 'phone',
+    'Ноутбук 💻': 'laptop',
+    'Компютер 🖥️': 'computer',
+    'Вейп 🚬': 'vape',
+    'Другое ': 'other'
+}
+
+
+def devices_list_markup() -> InlineKeyboardMarkup:
+    """ Returns inline keyboard with devices list """
+    buttons = []
+    current_row = []
+    row_length = 2
+    
+    for device, callback in DEVICES.items():
+        button = InlineKeyboardButton(text=device, callback_data=callback)
+        current_row.append(button)
+        
+        # Adding a row of buttons when the len of row is equal to "row_lenght"
+        if len(current_row) == row_length:
+            buttons.append(current_row)
+            current_row = []
+    
+    # Add the last row if the rest is empty
+    buttons.append(current_row) if current_row is not None else None
+    
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
