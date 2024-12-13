e589f95e0d7b0fab5d5afeab8efcb1eafbb535e1
Jeywin <j3ywin@gmail.com>
2024-12-13 14:46:09 +0300

    Enhance /start command

--- handlers/start.py
+++ handlers/start.py
@@ -2,21 +2,22 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
 
-from markups import(
-    devices_list_markup
+from markups import (
+    main_menu_markup
 )
 
 
 start_router = Router(name=__name__)
 
-MESSAGE_TEXT = '👋 Здравствуйте! Мы готовы помочь вам с ремонтом.\n' \
-               'Пожалуйста, выберите устройство, которое нуждается в ' \
-               'обслуживании, и мы обработаем ваш запрос.' 
+MESSAGE_TEXT = '👋 *Добро пожаловать!*\n'\
+               'Вы попали в бот для оформления заявок на ремонт.\n'\
+               'Здесь вы можете быстро и удобно описать вашу проблему, и наш специалист свяжется с вами в ближайшее время.\n\n'\
+               'Чтобы создать _новую заявку_, пожалуйста, используйте *меню* внизу.'
 
 
 @start_router.message(Command('start'))
-async def start_command(msg: Message) -> None:
-    await msg.answer(
+async def start_command(message: Message) -> None:
+    await message.answer(
         text=MESSAGE_TEXT,
-        reply_markup=devices_list_markup()
+        reply_markup=main_menu_markup()
     )
