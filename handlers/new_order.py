2870f5ec2b38013cb0620ae1401a178bbfedd485
Jeywin <j3ywin@gmail.com>
2024-12-13 14:46:26 +0300

    Implement new_order handler

--- handlers/new_order.py
+++ handlers/new_order.py
@@ -1,10 +1,19 @@
 from aiogram import Router, F
 from aiogram.types import Message
+from aiogram.filters import Command
 
+from markups import devices_list_markup
 
 new_order_router = Router(name=__name__)
 
+MESSAGE_TEXT = 'Пожалуйста, выберите устройство, которое нуждается в ' \
+               'обслуживании, и мы обработаем ваш запрос.' 
+
 
 @new_order_router.message(F.text == 'Новый заказ 📬')
+# @new_order_router.message(Command('new_order'))
 async def new_order_button_handler(message: Message):
-    pass
+    await message.answer(
+        text=MESSAGE_TEXT,
+        reply_markup=devices_list_markup()
+    )
