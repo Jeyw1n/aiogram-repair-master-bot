c31bdc3d6e9641834fca76ab1ffdf3dcc8a59c33
Jeywin <j3ywin@gmail.com>
2024-12-13 17:48:42 +0300

    Fix date format

--- database/models.py
+++ database/models.py
@@ -8,7 +8,7 @@ class Order:
         self.device_type = device_type
         self.device_name = device_name
         self.description = description
-        self.created_at = created_at or datetime.now().strftime("%Y-%m-%d %H:%M:%S") # Автоматическое заполнение
+        self.created_at = created_at or datetime.now().strftime("%m.%d.%Y %H:%M:%S") # Автоматическое заполнение
 
     def __repr__(self):
         return f"Order(user_id={self.user_id}, device_type='{self.device_type}', device_name='{self.device_name}', description='{self.description}', created_at='{self.created_at}')"
