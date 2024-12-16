6e42520354a1965dd5598d99213d577d447e08e1
Jeywin <j3ywin@gmail.com>
2024-12-16 15:24:38 +0300

    Add status field to Order model

--- database/models.py
+++ database/models.py
@@ -2,11 +2,12 @@ from datetime import datetime
 
 
 class Order:
-    def __init__(self, user_id, device_type, device_name, description, created_at=None):
+    def __init__(self, user_id, device_type, device_name, description, status=0, created_at=None):
         self.user_id = user_id
         self.device_type = device_type
         self.device_name = device_name
         self.description = description
+        self.status = status 
         self.created_at = created_at or datetime.now().strftime("%m.%d.%Y %H:%M:%S") # Автоматическое заполнение
 
     def __repr__(self):
