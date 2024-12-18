a19f7d23c035002f19b01e6cdc7b1a5911015649
Jeywin <j3ywin@gmail.com>
2024-12-18 22:20:06 +0300

    Remake paginator markup

--- markups/paginator.py
+++ markups/paginator.py
@@ -4,25 +4,19 @@ from aiogram.types import (
 )
 
 
-def paginator_markup(current_page: int, page_size: int, total_pages: int) -> InlineKeyboardMarkup:
+def paginator_markup(current_page: int, total_pages: int) -> InlineKeyboardMarkup:
     """Create an inline keyboard markup for pagination."""
     buttons: list[InlineKeyboardButton] = []
-    
-    previous_page = current_page - 1
-    next_page = current_page + 1
-    
-    previous_page_callback = f'paginator_{previous_page}_{page_size}'
-    next_page_callback = f'paginator_{next_page}_{page_size}'
 
-    previous_page_button = InlineKeyboardButton(text='<', callback_data=previous_page_callback)
-    next_page_button = InlineKeyboardButton(text='>', callback_data=next_page_callback)
+    # Create previous and next page callbacks
+    if current_page > 0:  # If not on the first page
+        previous_page_callback = f'paginator_{current_page - 1}'
+        previous_page_button = InlineKeyboardButton(text='⏪', callback_data=previous_page_callback)
+        buttons.append(previous_page_button)
 
-    if previous_page_button < 0:
+    if current_page < total_pages - 1:  # If not on the last page
+        next_page_callback = f'paginator_{current_page + 1}'
+        next_page_button = InlineKeyboardButton(text='⏩', callback_data=next_page_callback)
         buttons.append(next_page_button)
-    elif next_page_button > total_pages:
-        buttons.append(previous_page_button)
-    else:
-        buttons.append(previous_page_button)
-        buttons.append(next_page_button)
-    
+
     return InlineKeyboardMarkup(inline_keyboard=[buttons])
