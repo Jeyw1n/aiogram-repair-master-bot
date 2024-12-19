d2de4c1d06c1d61e2c20a47a98b6d4a254e5bc52
Jeywin <j3ywin@gmail.com>
2024-12-19 16:15:26 +0300

    Add order_id attribute to Order class

--- database/models.py
+++ database/models.py
@@ -2,13 +2,11 @@ from datetime import datetime
 
 
 class Order:
-    def __init__(self, user_id, device_type, device_name, description, status=0, created_at=None):
+    def __init__(self, user_id, device_type, device_name, description, status=0, created_at=None, order_id=None):
         self.user_id = user_id
         self.device_type = device_type
         self.device_name = device_name
         self.description = description
         self.status = status 
-        self.created_at = created_at or datetime.now().strftime("%m.%d.%Y %H:%M:%S") # Автоматическое заполнение
-
-    def __repr__(self):
-        return f"Order(user_id={self.user_id}, device_type='{self.device_type}', device_name='{self.device_name}', description='{self.description}', created_at='{self.created_at}')"
+        self.created_at = created_at or datetime.now().strftime("%m.%d.%Y %H:%M:%S")
+        self.order_id = order_id
